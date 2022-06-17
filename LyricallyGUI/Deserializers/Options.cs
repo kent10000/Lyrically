aaa47046dc625ad8468055c8dabe50d992929e8e
Aaron <aaronrichards4321@gmail.com>
2022-06-16 22:21:42 -0400

    Add files via upload

---
+++ LyricallyGUI/Deserializers/Options.cs
@@ -0,0 +1,25 @@
+﻿namespace LyricallyGUIOptions.Deserializers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Options
+    {
+        [JsonProperty("SpotifyToken")]
+        public object SpotifyToken { get; set; }
+
+        [JsonProperty("GeniusToken")]
+        public object GeniusToken { get; set; }
+
+        [JsonProperty("UserExpUI")]
+        public bool UserExpUi { get; set; }
+        
+        [JsonProperty("SpotifyAuth")]
+        
+        public object SpotifyAuth { get; set; }
+    }
+}
